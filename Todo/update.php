9506f6645db64734e37dab1565437c4513f491e9
Aleksandrgithub <drozdov.aleksandr123@gmail.com>
2021-04-30 10:48:23 -0700

    fix request status

--- Todo/update.php
+++ Todo/update.php
@@ -5,9 +5,11 @@ header("Access-Control-Allow-Methods: POST");
 
 include '../Database/transaction.php';
 include '../Model/task.php';
+include '../Transport/response.php';
 
 use Model\Task\Task as Task;
-use Model\Task\Status as Status;
+use Transport\Response\Status as ResponseStatus;
+use Model\Task\Status as TaskStatus;
 use Database\Transaction as Transaction;
 
 $transaction = new Transaction();
@@ -17,32 +19,32 @@ $data = json_decode(file_get_contents("php://input"));
 if(isset($data->id) && isset($data->status)) {
 	$id = $data->id;
 	$status = $data->status;
-	if($status == Status::COMPLETED) {
+	if($status == TaskStatus::COMPLETED) {
 		$task->setId($id);
 		$task->setStatusCompleted($status);
 		if($transaction->checkTaskExist($id)) {
 			if($transaction->checkTaskStatus($id)) {
 				if ($transaction->update($task)) {
-					http_response_code(200);
+					http_response_code(ResponseStatus::HTTP_OK);
 					echo json_encode(array("message" => "Task has been updated."), JSON_UNESCAPED_UNICODE);
 				} else {
-					http_response_code(500);
+					http_response_code(ResponseStatus::HTTP_INTERNAL_SERVER_ERROR);
 					echo json_encode(array("message" => "Unable to update task."), JSON_UNESCAPED_UNICODE);
 				}
 			} else {
-				http_response_code(400);
+				http_response_code(ResponseStatus::HTTP_BAD_REQUEST);
 				echo json_encode(array("message" => "Unable to update the task. The task is already marked as completed."), JSON_UNESCAPED_UNICODE);
 			}
 		} else {
-			http_response_code(400);
+			http_response_code(ResponseStatus::HTTP_BAD_REQUEST);
 			echo json_encode(array("message" => "Unable to update the task. There is no task with this number."), JSON_UNESCAPED_UNICODE);
 		}
 	} else {
-		http_response_code(400);
+		http_response_code(ResponseStatus::HTTP_BAD_REQUEST);
 		echo json_encode(array("message" => "There was an error in the request. Status value of 'completed' task = 0"), JSON_UNESCAPED_UNICODE);
 	}
 } else {
-	http_response_code(400);
+	http_response_code(ResponseStatus::HTTP_BAD_REQUEST);
 	echo json_encode(array("message" => "Unable to update task. Error in the request, indicate the number and status of the task to be updated in the request"), JSON_UNESCAPED_UNICODE);
 }
 
